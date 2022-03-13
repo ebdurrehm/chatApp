f098a77f6a3c336c908ba854fd5277872001986b
Abdurrahim Ahmadov <ebdurrehim5@gmail.com>
2022-03-13 22:31:57 +0400

    add socket io to project 1) Add  socket.io for handling communication between server and client 2) add mongostore for saving sesssion information on the mongodb 3) add passport.socketio for checking who is connected the server 4) create chat page and implement socket.io for enabling real-time    communication

--- server.js
+++ server.js
@@ -4,14 +4,31 @@ const express = require('express');
 const session = require('express-session');
 const myDB = require('./connection');
 const auth = require('./auth');
-const router = require('./router');
+const routes = require('./routes');
 const passport = require('passport');
 const app = express();
-
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+const MongoStore = require('connect-mongo')(session);
+const URI = process.env.MONGO_URI;
+const store = new MongoStore({ url: URI });
+const passportSocketIo = require('passport.socketio');
+const cookieParser = require('cookie-parser');
 
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+io.use(
+  passportSocketIo.authorize({
+    cookieParser: cookieParser,
+    key: 'express.sid',
+    secret:process.env.SESSION_SECRET,
+    store: store,
+    success: onAuthorizeFail,
+    fail: onAuthorizeSuccess
+    
+  })
+)
 
 const option = {
   secret: process.env.SESSION_SECRET,
@@ -19,6 +36,19 @@ const option = {
   saveUninitialized: true,
   cookie: {secure:false}
 };
+
+function onAuthorizeSuccess(data, accept) {
+  console.log('successful connection to socket.io');
+
+  accept(null, true);
+}
+
+
+function onAuthorizeFail(data, message, error, accept) {
+  if (error) throw new Error(message);
+  console.log('failed connection to socket.io:', message);
+  accept(null, false);
+}
 
 app.use(session(option));
 
@@ -30,9 +60,19 @@ app.set('view engine','pug');
 
 myDB(async (client) => {
   const myDataBase = await client.db('database').collection('users');
-   router(app, myDataBase);
+   routes(app, myDataBase);
    auth(app,myDataBase);
-      
+  let currentUsers = 0;
+  io.on('connection', socket => {
+  console.log('user ' + socket.request.user.name + ' connected');
+    ++currentUsers;
+    io.emit('user count', currentUsers);
+    socket.on('disconnect',()=>{
+      console.log("a user has disconnected");
+      --currentUsers;
+      io.emit('user count', currentUsers);
+    })
+});
 
   // Be sure to add this...
 }).catch((e) => {
@@ -42,6 +82,6 @@ myDB(async (client) => {
 });
 // app.listen out here...
 
-app.listen(process.env.PORT || 3000, () => {
+http.listen(process.env.PORT || 3000, () => {
   console.log('Listening on port ' + process.env.PORT);
 });
